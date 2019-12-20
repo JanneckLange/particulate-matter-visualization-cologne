ce7154016c063c5fb3917928b98f8da118b0cb0a
Richard <richard.musebrink@gmail.com>
2019-12-20 08:15:11 +0100

    csv import example

--- public/time_series_many/js/main.js
+++ public/time_series_many/js/main.js
@@ -1,24 +1,29 @@
 am4core.ready(function() {
 
-// Themes begin
     am4core.useTheme(am4themes_animated);
-// Themes end
+    am4core.options.minPolylineStep = 5;
 
     var chart = am4core.create("chartdiv", am4charts.XYChart);
     chart.paddingRight = 20;
+    chart.dataSource.url = "../data/test_refactored.csv";
+    chart.dataSource.parser = new am4core.CSVParser();
+    chart.dataSource.parser.options.useColumnNames = true;
+    chart.dataSource.parser.options.delimiter = ";";
+    chart.dateFormatter.inputDateFormat = "i";
 
-    var data = [];
+
+    /*var data = [];
     var visits = 10;
     for (var i = 1; i < 50000; i++) {
         visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
         data.push({ date: new Date(2018, 0, i), value: visits });
     }
 
-    chart.data = data;
+    chart.data = data;*/
 
     var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
     dateAxis.renderer.grid.template.location = 0;
-    dateAxis.minZoomCount = 5;
+    dateAxis.minZoomCount = 1;
 
 
 // this makes the data to be grouped
@@ -28,8 +33,8 @@ am4core.ready(function() {
     var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
     var series = chart.series.push(new am4charts.LineSeries());
-    series.dataFields.dateX = "date";
-    series.dataFields.valueY = "value";
+    series.dataFields.dateX = "timestamp";
+    series.dataFields.valueY = "P2";
     series.tooltipText = "{valueY}";
     series.tooltip.pointerOrientation = "vertical";
     series.tooltip.background.fillOpacity = 0.5;
