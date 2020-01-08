4a22dae0242b5ed3f2cc25f8e2b7c02f2d231170
lange <lange@infoboard.biz>
2020-01-09 00:28:31 +0100

    log with timestamp

--- api/app.js
+++ api/app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('log-timestamp');
 
 var indexRouter = require('./routes');
-
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger(':date[iso] - :method :url :status - :response-time ms'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
