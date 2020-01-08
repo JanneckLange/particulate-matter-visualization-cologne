d0b131f3f835832484ec9e77b99d602b2ee438c9
lange <lange@infoboard.biz>
2020-01-08 13:50:12 +0100

    add send everything route

--- public/mongo/api/routes/index.js
+++ public/mongo/api/routes/index.js
@@ -21,7 +21,9 @@ router.get('/', function (req, res, next) {
     let sensor = req.query.sensor;
 
     //check if sensor exists
-    if (!sensors.includes(parseInt(sensor))) {
+    if (!min && !max && !sensor) {
+        sendEverything(req, res, next);
+    } else if (!sensors.includes(parseInt(sensor))) {
         res.send('sensor not available').status(400);
     } else if (min > max) {
         res.send('min must be lower than max').status(400);
@@ -116,6 +118,19 @@ function sendAverageDataTime(req, res, next) {
             sensor: parseInt(sensor)
         };
         dbo.collection('dailyAVG').find(query).toArray(function (err, result) {
+            if (err) throw err;
+            res.send(result);
+            db.close();
+        });
+    });
+}
+
+function sendEverything(req, res, next) {
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        let dbo = db.db("gdv");
+
+        dbo.collection('dailyAVG').find().toArray(function (err, result) {
             if (err) throw err;
             res.send(result);
             db.close();
