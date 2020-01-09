a89ae4ab92c53b5e22b61944a485fd88d503a9c4
Richard <richard.musebrink@gmail.com>
2020-01-09 21:41:43 +0100

    refactored

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,84 @@ am4core.ready(async function () {
     am4core.options.minPolylineStep = 10;
     am4core.options.queue = true;
     am4core.options.onlyShowOnViewport = true;
-    const path = "http://996d6c12.ngrok.io/api";
+    const path = "http://94b3f9fa.ngrok.io/api";
+
+
+    let mainContainer;
+    let chart;
+    let dateAxis;
+    let valueAxis;
+    let slider;
+
+    let accuracy = "averageDataTime";
+    let currentMin;
+    let currentMax;
+    let activeSensors = [];
+
+    function initMainContainer() {
+        mainContainer = am4core.create("chartdiv", am4core.Container);
+        mainContainer.background.fillOpacity = 0.3;
+        mainContainer.background.fill = am4core.color("#79b8df");
+        mainContainer.width = am4core.percent(100);
+        mainContainer.height = am4core.percent(100);
+    }
+
+    async function initChart() {
+        // CREATE CHART ------------------------------------------------------------------------------------------------
+        chart = mainContainer.createChild(am4charts.XYChart);
+        chart.events.on("up", () => {
+            // necessary because min/max zoom level updates slow
+            setTimeout(() => updateData(), 200);
+        });
+        // -------------------------------------------------------------------------------------------------------------
+
+        // CREATE AXES -------------------------------------------------------------------------------------------------
+        dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+        valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        dateAxis.dateFormatter.dateFormat = "d MMM, yyyy";
+        dateAxis.groupData = true;
+        // -------------------------------------------------------------------------------------------------------------
+
+        // CREATE SERIES -----------------------------------------------------------------------------------------------
+        let sensors = await getSensors();
+        sensors = sensors.sensors;
+
+        for (let i = 0; i < sensors.length; i++) {
+            let series = chart.series.push(new am4charts.LineSeries());
+            series.dataFields.valueY = "P2";
+            series.dataFields.dateX = "timestamp";
+            series.tooltipText = "{dateX.formatDate('yyyy-mm-dd')}: P2: {valueY.formatNumber('#.00')}";
+            series.strokeWidth = 2;
+            series.opacity = 0.7;
+            series.name = sensors[i].toString();
+            series.id = sensors[i];
+            series.dataSource.updateCurrentData = false;
+            series.dataSource.url = path + "/?sensor=" + sensors[i];
+            series.resolution = "";
+            series.minBulletDistance = 30;
+            series.hide();
+
+            series.dataSource.events.on("done", (ev) => {
+                series.data = ev.data[0].data;
+                series.resolution = ev.data[0].resolution;
+            });
+        }
+        // -------------------------------------------------------------------------------------------------------------
+
+        // CREATE CURSOR -----------------------------------------------------------------------------------------------
+        chart.cursor = new am4charts.XYCursor();
+        chart.cursor.fullWidthLineX = true;
+        chart.cursor.xAxis = dateAxis;
+        chart.cursor.lineX.strokeOpacity = 0;
+        chart.cursor.lineX.fill = am4core.color("#000");
+        chart.cursor.lineX.fillOpacity = 0.1;
+        // -------------------------------------------------------------------------------------------------------------
+
+        // CREATE SLIDER -----------------------------------------------------------------------------------------------
+        slider = mainContainer.createChild(am4core.Slider);
+
+        // -------------------------------------------------------------------------------------------------------------
+    }
 
     async function getData(min, max, sensor_id) {
         let res = await fetch(path + "/?min=" + min + "&max=" + max + "&sensor=" + sensor_id);
@@ -18,67 +95,6 @@ am4core.ready(async function () {
         let res = await fetch(path + "/info");
         return await res.json();
     }
-
-    var chart = am4core.create("chartdiv", am4charts.XYChart);
-    var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-    dateAxis.dateFormatter.dateFormat = "d MMM, yyyy";
-
-    let accuracy = "averageDataTime";
-    let currentMin;
-    let currentMax;
-
-    dateAxis.groupData = true;
-
-    let sensors = await getSensors();
-    sensors = sensors.sensors;
-
-    let activeSensors = [];
-
-    for (let i = 0; i < sensors.length; i++) {
-        let series = chart.series.push(new am4charts.LineSeries());
-        series.dataFields.valueY = "P2";
-        series.dataFields.dateX = "timestamp";
-        series.tooltipText = "{dateX.formatDate('yyyy-mm-dd')}: P2: {valueY.formatNumber('#.00')}";
-        series.strokeWidth = 2;
-        series.opacity = 0.7;
-        series.name = sensors[i].toString();
-        series.id = sensors[i];
-        series.dataSource.updateCurrentData = false;
-        series.dataSource.url = path + "/?sensor=" + sensors[i];
-        series.resolution = "";
-        series.minBulletDistance = 30;
-
-        /*var bullet = series.bullets.push(new am4charts.CircleBullet());
-        bullet.circle.strokeWidth = 2;
-        bullet.circle.radius = 4;
-        bullet.circle.fill = am4core.color("#fff");
-        var bullethover = bullet.states.create("hover");
-        bullethover.properties.scale = 1.3;*/
-
-        series.dataSource.events.on("done", (ev) => {
-            series.data = ev.data[0].data;
-            series.resolution = ev.data[0].resolution;
-            console.log(series.resolution);
-        });
-
-        series.hide();
-    }
-
-    console.log(activeSensors);
-
-    var scrollbarX = new am4core.Scrollbar();
-    scrollbarX.marginBottom = 20;
-    chart.scrollbarX = scrollbarX;
-    chart.scrollbarX.startGrip.disabled = true;
-    chart.scrollbarX.endGrip.disabled = true;
-    chart.scrollbarX.thumb.minWidth = 50;
-
-
-    chart.events.on("up", () => {
-        updateData();
-        console.log("chart touched")
-    });
 
     async function updateData() {
         if (activeSensors.length > 0) {
@@ -96,7 +112,7 @@ am4core.ready(async function () {
                 newAccuracy = "averageDataTime";
             }
 
-            if (newAccuracy !== accuracy || (min < currentMin || max > currentMax) && newAccuracy !== accuracy) {
+            if (newAccuracy !== accuracy || ((min < currentMin || max > currentMax) && newAccuracy !== accuracy)) {
                 console.log("updating data");
                 for (let i = 0; i < activeSensors.length; i++) {
                     let current = chart.map.getKey(activeSensors[i]);
@@ -129,34 +145,6 @@ am4core.ready(async function () {
         }
     }
 
-    /*chart.legend = new am4charts.Legend();
-    chart.legend.useDefaultMarker = true;
-    var marker = chart.legend.markers.template.children.getIndex(0);
-    marker.cornerRadius(12, 12, 12, 12);
-    marker.strokeWidth = 2;
-    marker.strokeOpacity = 1;
-    marker.stroke = am4core.color("#ccc");
-    chart.legend.itemContainers.template.events.on("hit", (ev) => {
-        let sensor_id = ev.target.dataItem.dataContext.name;
-        //somehow status updates to late..so we have to ask for the opposite -.-
-        if (!ev.target.dataItem.dataContext.isHidden) {
-            //console.log(sensor_id + " is hidden");
-            makeInactive(sensor_id);
-        } else {
-            //console.log(sensor_id + " is visible");
-            makeActive(sensor_id);
-        }
-    });*/
-
-    // Add cursor
-    chart.cursor = new am4charts.XYCursor();
-    chart.cursor.fullWidthLineX = true;
-    chart.cursor.xAxis = dateAxis;
-    chart.cursor.lineX.strokeOpacity = 0;
-    chart.cursor.lineX.fill = am4core.color("#000");
-    chart.cursor.lineX.fillOpacity = 0.1;
-
-
     //MAP --------------------------------------------------------------------------------------------------------------
     function makeIcon(sensor_id, active) {
         let color;
@@ -188,7 +176,6 @@ am4core.ready(async function () {
         });
     }
 
-
     const mymap = L.map('map', {
         dragging: false,
         scrollWheelZoom: false,
@@ -209,7 +196,7 @@ am4core.ready(async function () {
             active: false
         };
 
-        marker.on('click', (x) => {
+        marker.on('click', async (x) => {
             let sensor_id = x.target.options.title;
             if (marker.gdvStats.active) {
                 x.target.setIcon(makeIcon(sensor_id, false));
@@ -221,6 +208,14 @@ am4core.ready(async function () {
                 x.target.setIcon(makeIcon(sensor_id, true));
                 makeActive(sensor_id);
                 let current = chart.map.getKey(sensor_id);
+                if (current.resolution !== accuracy) {
+                    let basic = chart.map.getKey(activeSensors[0]);
+                    let min = basic.xAxis.minZoomed;
+                    let max = basic.xAxis.maxZoomed;
+                    console.log("series needs update!");
+                    let data = await getData(min, max, sensor_id);
+                    current.data = data.data
+                }
                 current.show();
                 marker.gdvStats.active = true;
             }
@@ -232,6 +227,10 @@ am4core.ready(async function () {
         minZoom: 13,
         id: 'mapbox/streets-v11'
     }).addTo(mymap);
+
+    initMainContainer();
+    initChart()
+
 // ---------------------------------------------------------------------------------------------------------------------
 }, "chartdiv", am4charts.XYChart);
 // end am4core.ready()
