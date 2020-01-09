ed4afe4097f49d0f2bf108491b0a8daf35405891
lange <lange@infoboard.biz>
2020-01-09 01:35:25 +0100

    comments, refactor, performance

--- api/routes/index.js
+++ api/routes/index.js
@@ -21,6 +21,19 @@ const mongoProjection = {
         dataType: 0
     }
 };
+const mongoConnectionOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+let dbo;
+
+function startDatabase() {
+    MongoClient.connect(url, mongoConnectionOptions, function (err, db) {
+        if (err) throw err;
+        dbo = db.db("gdv");
+        console.log('Database Connected')
+    });
+}
 
 router.get('/', function (req, res, next) {
     let min = req.query.min;
@@ -31,130 +44,115 @@ router.get('/', function (req, res, next) {
     if (!sensor || !sensors.includes(parseInt(sensor))) {
         res.send('sensor not available').status(400);
     } else if (!min && !max) {//send full sensor data when no min/max is given
-        sendEverything(req, res, next, {sensor_id: parseInt(sensor)})
+        sendEverything(req, res, {sensor_id: parseInt(sensor)})
     } else if (min > max) {
         res.send('min must be lower than max').status(400);
     } else if (max - min < accurateDataTime) {//send hourly data when time range is smaller than accurateDataTime
         console.log('send accurate Data');
-        sendAccurateDataTime(req, res, next);
+        sendAccurateDataTime(req, res);
     } else if (max - min < hourlyDataTime) {//send daily data when time range is smaller than hourlyDataTime
         console.log('send average Data (hour)');
-        sendAverageDataTime(req, res, next, 'hourlyAVG');
+        sendAverageDataTime(req, res, 'hourlyAVG');
     } else {
         console.log('send average Data (day)');
-        sendAverageDataTime(req, res, next, 'dailyAVG');
+        sendAverageDataTime(req, res, 'dailyAVG');
     }
 });
 
-// router.get('/accurate', function (req, res, next) {
-//     sendAccurateDataTime(req, res, next);
-// });
-//
-// router.get('/average/hour', function (req, res, next) {
-//     sendAverageDataTime(req, res, next, 'hourlyAVG');
-// });
-//
-// router.get('/average/day', function (req, res, next) {
-//     sendAverageDataTime(req, res, next, 'dailyAVG');
-// });
 
 router.get('/info', function (req, res, next) {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        let dbo = db.db("gdv").collection('dailyAVG');
-
-        MongoClient.connect(url, function (err, db) {
-            dbo.distinct('sensor_id').then((sensorData) => {
-                dbo.find().sort({timestamp: 1}).limit(1).toArray((err, minData) => {
-                    dbo.find().sort({timestamp: -1}).limit(1).toArray((err, maxData) => {
-                        res.send({
-                            sensors: sensorData,
-                            min: minData[0].timestamp,
-                            max: maxData[0].timestamp,
-                            autoAccuracy: {
-                                accurate: hourlyDataTime,
-                                hourlyAverage: accurateDataTime,
-                                dailyAverage: null,
-                                description: "send data when time range between min and max timestamp is smaller than given value (milliseconds). Null value means everything else."
-                            }
-                        })
-                    });
+    dbo.collection('dailyAVG').distinct('sensor_id').then((sensorData) => {
+        dbo.collection('dailyAVG').find().sort({timestamp: 1}).limit(1).toArray((err, minData) => {
+            dbo.collection('dailyAVG').find().sort({timestamp: -1}).limit(1).toArray((err, maxData) => {
+                res.send({
+                    sensors: sensorData,
+                    min: minData[0].timestamp,
+                    max: maxData[0].timestamp,
+                    autoAccuracy: {
+                        accurate: hourlyDataTime,
+                        hourlyAverage: accurateDataTime,
+                        dailyAverage: null,
+                        description: "send data when time range between min and max timestamp is smaller than given value (milliseconds). Null value means everything else."
+                    }
                 });
-            })
-        });
+            });
+        })
     });
 });
 
-function sendAccurateDataTime(req, res, next) {
+/**
+ * send original sensor data
+ * @param req
+ * @param res
+ */
+function sendAccurateDataTime(req, res) {
     let sensor = req.query.sensor;
     let min = req.query.min;
     let max = req.query.max;
 
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        let dbo = db.db("gdv");
-
-        let query = {
-            timestamp: {
-                $gte: parseInt(min),//min Date
-                $lt: parseInt(max)//max Date
-            }
-        };
-        dbo
-            .collection("s" + sensor)
-            .find(query, mongoProjection)
-            .sort({timestamp: 1})
-            .toArray(function (err, result) {
-                if (err) throw err;
-                res.send({resolution: 'accurateDataTime', data: result});
-                db.close();
-            });
-    });
+    let query = {
+        timestamp: {
+            $gte: parseInt(min),//min Date
+            $lt: parseInt(max)//max Date
+        }
+    };
+    dbo
+        .collection("s" + sensor)
+        .find(query, mongoProjection)
+        .sort({timestamp: 1})
+        .toArray(function (err, result) {
+            if (err) throw err;
+            res.send({resolution: 'accurateDataTime', data: result});
+        });
 }
 
-function sendAverageDataTime(req, res, next, accuracy) {
+/**
+ * send daily or hourly average for time range
+ * @param req
+ * @param res
+ * @param accuracy string 'hourlyAVG' or 'dailyAVG'
+ */
+function sendAverageDataTime(req, res, accuracy) {
     let sensor = req.query.sensor;
     let min = req.query.min;
     let max = req.query.max;
 
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        let dbo = db.db("gdv");
 
-        let query = {
-            timestamp: {
-                $gte: parseInt(min),//min Date
-                $lt: parseInt(max)//max Date
-            },
-            sensor_id: parseInt(sensor)
-        };
-        dbo
-            .collection(accuracy)
-            .find(query, mongoProjection)
-            .sort({timestamp: 1})
-            .toArray(function (err, result) {
-                if (err) throw err;
-                res.send({resolution: accuracy === 'hourlyAVG' ? 'hourlyDataTime' : 'averageDataTime', data: result});
-                db.close();
-            });
-    });
+    let query = {
+        timestamp: {
+            $gte: parseInt(min),//min Date
+            $lt: parseInt(max)//max Date
+        },
+        sensor_id: parseInt(sensor)
+    };
+    dbo
+        .collection(accuracy)
+        .find(query, mongoProjection)
+        .sort({timestamp: 1})
+        .toArray(function (err, result) {
+            if (err) throw err;
+            res.send({resolution: accuracy === 'hourlyAVG' ? 'hourlyDataTime' : 'averageDataTime', data: result});
+        });
 }
 
-function sendEverything(req, res, next, query) {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        let dbo = db.db("gdv");
-
-        dbo
-            .collection('dailyAVG')
-            .find(query, mongoProjection)
-            .sort({timestamp: 1})
-            .toArray(function (err, result) {
-                if (err) throw err;
-                res.send({resolution: 'averageDataTime', data: result});
-                db.close();
-            });
-    });
+/**
+ * send daily average for query
+ * @param req
+ * @param res
+ * @param query for mongo search
+ */
+function sendEverything(req, res, query) {
+    dbo
+        .collection('dailyAVG')
+        .find(query, mongoProjection)
+        .sort({timestamp: 1})
+        .toArray(function (err, result) {
+            if (err) throw err;
+            res.send({resolution: 'averageDataTime', data: result});
+        });
 }
 
-module.exports = router;
+module.exports = {
+    router: router,
+    dbStarter: startDatabase()
+};
