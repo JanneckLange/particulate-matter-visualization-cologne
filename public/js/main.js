cccb104e311292128b4acba3058fab3c4abdec42
riskysciolism <richard.musebrink@gmail.com>
2020-01-14 15:54:39 +0100

    refactored

--- public/js/main.js
+++ public/js/main.js
@@ -2,20 +2,27 @@ am4core.ready(async function () {
     am4core.options.minPolylineStep = 10;
     am4core.options.queue = true;
     am4core.options.onlyShowOnViewport = true;
-    const path = "http://94b3f9fa.ngrok.io/api";
+    const path = "http://e3e1402c.ngrok.io/api";
 
 
     let mainContainer;
     let chart;
-    let dateAxis;
-    let valueAxis;
+    let dirtDateAxis;
+    let weatherDateAxis;
+    let dirtAxis;
+    let weatherAxis;
     let slider;
-    let mymap;
+    let myMap;
+    let sensors = await getSensors();
+
 
     let accuracy = "averageDataTime";
     let currentMin;
     let currentMax;
     let activeSensors = [];
+
+    let currentWeatherMin;
+    let currentWeatherMax;
 
     function initMainContainer() {
         mainContainer = am4core.create("chartdiv", am4core.Container);
@@ -28,6 +35,8 @@ am4core.ready(async function () {
     async function initChart() {
         // CREATE CHART ------------------------------------------------------------------------------------------------
         chart = mainContainer.createChild(am4charts.XYChart);
+        chart.scrollbarX = new am4core.Scrollbar();
+        chart.zoomOutButton.disabled = true;
         chart.events.on("up", () => {
             // necessary because min/max zoom level updates slow
             setTimeout(() => updateData(), 200);
@@ -35,15 +44,54 @@ am4core.ready(async function () {
         // -------------------------------------------------------------------------------------------------------------
 
         // CREATE AXES -------------------------------------------------------------------------------------------------
-        dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-        valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-        dateAxis.dateFormatter.dateFormat = "d MMM, yyyy";
-        dateAxis.groupData = true;
+        weatherDateAxis = chart.xAxes.push(new am4charts.DateAxis());
+        dirtDateAxis = chart.xAxes.push(new am4charts.DateAxis());
+        dirtDateAxis.baseInterval = weatherDateAxis.baseInterval = {
+            "timeUnit": "minute",
+            "count": 1
+        };
+        weatherDateAxis.min = sensors.min;
+        weatherDateAxis.max = sensors.max;
+        weatherDateAxis.cursorTooltipEnabled = false;
+        weatherDateAxis.hidden = true;
+
+        dirtAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        weatherAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        weatherAxis.renderer.opposite = true;
+        weatherAxis.renderer.cellStartLocation = 0;
+        weatherAxis.renderer.cellEndLocation = 1;
+        weatherAxis.min = 0;
+
+        dirtDateAxis.dateFormatter.dateFormat = "d MMM, yyyy";
+        dirtDateAxis.groupData = true;
+
+        weatherDateAxis.groupData = true;
+
         // dateAxis.baseInterval = {count: 1, timeUnit: "day"};
         // -------------------------------------------------------------------------------------------------------------
 
-        // CREATE SERIES -----------------------------------------------------------------------------------------------
-        let sensors = await getSensors();
+        // CREATE WEATHER SERIES ---------------------------------------------------------------------------------------
+        let weather = chart.series.push(new am4charts.ColumnSeries());
+        weather.strokeWidth = 3;
+        weather.fillOpacity = 0.5;
+        weather.dataFields.valueY = "Niederschlag";
+        weather.dataFields.dateX = "date";
+        weather.dataSource.url = path + "/weather";
+        weather.yAxis = weatherAxis;
+        weather.xAxis = weatherDateAxis;
+        weather.name = "weather";
+        weather.id = "weather";
+        weather.groupFields.valueY = "sum";
+
+        weather.dataSource.events.on("done", (ev) => {
+            ev.data[0].data.P2 += 3600000;
+            weather.data = ev.data[0].data;
+            weather.resolution = ev.data[0].type;
+            weather.invalidateRawData();
+        });
+        // -------------------------------------------------------------------------------------------------------------
+
+        // CREATE DIRT SERIES -----------------------------------------------------------------------------------------------
         sensors = sensors.sensors;
 
         for (let i = 0; i < sensors.length; i++) {
@@ -53,38 +101,61 @@ am4core.ready(async function () {
             series.tooltipText = "{dateX.formatDate('yyyy-mm-dd')}: P2: {valueY.formatNumber('#.00')}";
             series.strokeWidth = 2;
             series.opacity = 0.7;
+            series.yAxis = dirtAxis;
+            series.xAxis = dirtDateAxis;
             series.name = sensors[i].toString();
             series.id = sensors[i];
             series.dataSource.updateCurrentData = false;
             series.dataSource.url = path + "/?sensor=" + sensors[i];
             series.resolution = "";
-            series.minBulletDistance = 30;
             series.hide();
 
+            series.groupFields.valueY = "average";
             series.dataSource.events.on("done", (ev) => {
                 series.data = ev.data[0].data;
                 series.resolution = ev.data[0].resolution;
             });
         }
+
         // -------------------------------------------------------------------------------------------------------------
 
         // CREATE CURSOR -----------------------------------------------------------------------------------------------
         chart.cursor = new am4charts.XYCursor();
         chart.cursor.fullWidthLineX = true;
-        chart.cursor.xAxis = dateAxis;
+        chart.cursor.xAxis = dirtDateAxis;
         chart.cursor.lineX.strokeOpacity = 0;
         chart.cursor.lineX.fill = am4core.color("#000");
         chart.cursor.lineX.fillOpacity = 0.1;
+        chart.cursor.behavior = "panX";
         // -------------------------------------------------------------------------------------------------------------
 
-        // CREATE SLIDER -----------------------------------------------------------------------------------------------
-        slider = mainContainer.createChild(am4core.Slider);
+        let lowerRange = dirtAxis.axisRanges.create();
+        let middleRange = dirtAxis.axisRanges.create();
+        let upperRange = dirtAxis.axisRanges.create();
 
-        // -------------------------------------------------------------------------------------------------------------
+        lowerRange.value = 0;
+        lowerRange.endValue = 20;
+        lowerRange.axisFill.fill = am4core.color("#00b800");
+
+        middleRange.value = 20;
+        middleRange.endValue = 60;
+        middleRange.axisFill.fill = am4core.color("#ffff00");
+
+        upperRange.value = 60;
+        upperRange.endValue = 1000;
+        upperRange.axisFill.fill = am4core.color("#ff0000");
+
+        upperRange.axisFill.fillOpacity = middleRange.axisFill.fillOpacity = lowerRange.axisFill.fillOpacity = 0.1;
+        upperRange.grid.strokeOpacity = middleRange.grid.strokeOpacity = lowerRange.grid.strokeOpacity = 0;
     }
 
     async function getData(min, max, sensor_id) {
         let res = await fetch(path + "/?min=" + min + "&max=" + max + "&sensor=" + sensor_id);
+        return await res.json();
+    }
+
+    async function getWeatherData(min, max) {
+        let res = await fetch(path + "/weather/?min=" + min + "&max=" + max);
         return await res.json();
     }
 
@@ -99,6 +170,28 @@ am4core.ready(async function () {
     }
 
     async function updateData() {
+        // regarding weather ---------------------------
+        let weather = chart.map.getKey("weather");
+        let weatherMin = weather.xAxis.minZoomed;
+        let weatherMax = weather.xAxis.maxZoomed;
+        let deltaWeather = weatherMax - weatherMin;
+        let newWeatherAccuracy;
+        if (deltaWeather < 31 * 24 * 3600000) {
+            newWeatherAccuracy = "acc";
+        } else {
+            newWeatherAccuracy = "avg";
+        }
+        if (newWeatherAccuracy !== weather.resolution) {
+            console.log("updating weather data");
+            let data = await getWeatherData(weatherMin, weatherMax);
+            weather.data = data.data;
+            weather.resolution = newWeatherAccuracy;
+            currentMin = weatherMin;
+            currentMax = weatherMax;
+            weather.invalidateRawData();
+        }
+
+        // regarding dirt ---------------------------
         if (activeSensors.length > 0) {
             let basic = chart.map.getKey(activeSensors[0]);
             let min = basic.xAxis.minZoomed;
@@ -145,8 +238,8 @@ am4core.ready(async function () {
                 let current = activeSensors.shift();
                 chart.map.getKey(current).hide();
                 // change marker color
-                for(let i in mymap._layers) {
-                    let currentMarker = mymap._layers[i];
+                for(let i in myMap._layers) {
+                    let currentMarker = myMap._layers[i];
                     if(currentMarker.options.title === current) {
                         currentMarker.setIcon(makeIcon("", false));
                     }
@@ -186,7 +279,7 @@ am4core.ready(async function () {
         });
     }
 
-    mymap = L.map('map', {
+    myMap = L.map('map', {
         dragging: false,
         scrollWheelZoom: false,
         touchZoom: false
@@ -200,7 +293,7 @@ am4core.ready(async function () {
             title: sensorLocation._id,
             doubleClickZoom: false,
             icon: makeIcon(sensorLocation._id, false),
-        }).addTo(mymap);
+        }).addTo(myMap);
 
         marker.gdvStats = {
             active: false
@@ -236,11 +329,10 @@ am4core.ready(async function () {
         maxZoom: 13,
         minZoom: 13,
         id: 'mapbox/streets-v11'
-    }).addTo(mymap);
+    }).addTo(myMap);
 
     initMainContainer();
     initChart();
-    console.log(mymap);
 // ---------------------------------------------------------------------------------------------------------------------
 }, "chartdiv", am4charts.XYChart);
 // end am4core.ready()
